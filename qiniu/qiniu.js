1146e7263d02f87760a385d56de594463a3e972e
lizhizhou <lzz@meteroi.com>
2015-03-22 06:53:37 -0700

    upload file to qiniu

---
+++ qiniu/qiniu.js
@@ -0,0 +1,59 @@
+var qiniu = require("qiniu");
+
+qiniu.conf.ACCESS_KEY = 'IrDtWu7b7mBVDqSjLcek1kfbb3CM90JblgImlko6'
+qiniu.conf.SECRET_KEY = '1UlARj0pqeAiL_ipBLke1Gm_HBGNL60KDrSDjUdX'
+
+function uptoken(bucketname) {
+  var putPolicy = new qiniu.rs.PutPolicy(bucketname);
+  //putPolicy.callbackUrl = callbackUrl;
+  //putPolicy.callbackBody = callbackBody;
+  //putPolicy.returnUrl = returnUrl;
+  //putPolicy.returnBody = returnBody;
+  //putPolicy.asyncOps = asyncOps;
+  //putPolicy.expires = expires;
+
+  return putPolicy.token();
+}
+
+function uploadBuf(body, key, uptoken) {
+  var extra = new qiniu.io.PutExtra();
+  //extra.params = params;
+  //extra.mimeType = mimeType;
+  //extra.crc32 = crc32;
+  //extra.checkCrc = checkCrc;
+
+  qiniu.io.put(uptoken, key, body, extra, function(err, ret) {
+    if (!err) {
+      // 上传成功， 处理返回值
+      console.log(ret.key, ret.hash);
+      // ret.key & ret.hash
+    } else {
+      // 上传失败， 处理返回代码
+      console.log(err)
+      // http://developer.qiniu.com/docs/v6/api/reference/codes.html
+    }
+  });
+}
+
+function uploadFile(localFile, key, uptoken) {
+  var extra = new qiniu.io.PutExtra();
+  //extra.params = params;
+  //extra.mimeType = mimeType;
+  //extra.crc32 = crc32;
+  //extra.checkCrc = checkCrc;
+
+  qiniu.io.putFile(uptoken, key, localFile, extra, function(err, ret) {
+    if(!err) {
+      // 上传成功， 处理返回值
+      console.log(ret.key, ret.hash);
+      // ret.key & ret.hash
+    } else {
+      // 上传失败， 处理返回代码
+      console.log(err);
+      // http://developer.qiniu.com/docs/v6/api/reference/codes.html
+    }
+  });
+}
+
+uploadBuf("lizhizhou", "li.txt", uptoken("openfpgaduino"));
+
