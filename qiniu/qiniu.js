5943a1f92c34923f870bd9cac8a0cc91ccf78246
lizhizhou <lzz@meteroi.com>
2015-03-25 16:48:39 +0800

    add rest api

--- qiniu/qiniu.js
+++ qiniu/qiniu.js
@@ -97,6 +97,46 @@ http.get(options, function(res) {
         });
     });
 };
-
-
-
+/*
+var http = require('http');
+var equal = require('assert').equal;
+ 
+var username = 'falcon';
+var password = '';
+var _auth = 'Basic ' + new Buffer(username + ':' + password).toString('base64')
+ 
+var options = {
+    host: 'localhost',
+    port: 13080,
+    path: '/SM/7/rest/1.1/incident_list/',
+    method: 'GET',
+    headers:{
+        'accept': '*/*',
+        'content-type': "application/atom+xml",
+        'accept-encoding': 'gzip, deflate',
+        'accept-language': 'en-US,en;q=0.9',
+        'authorization': _auth,
+        'user-agent': 'nodejs rest client'
+    }
+};
+ 
+var req = http.request(options, function (res) {
+    console.log('STATUS: ' + res.statusCode);
+    equal(200, res.statusCode);
+    console.log('HEADERS: ' + JSON.stringify(res.headers));
+ 
+    res.on('data',function (chunk) {
+         console.log('BODY: ' + chunk);
+    });
+});
+ 
+req.on('error', function(e) {
+  console.log('problem with request: ' + e.message);
+});
+ 
+ 
+req.end();
+*/
+/*
+$ curl --data "build=true" -X POST https://registry.hub.docker.com/u/lizhizhou/cloudfpga/trigger/4dd21468-2d62-40e7-8ff3-98db4fc22a9a/
+*/
