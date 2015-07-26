6407cce434e57f54561c91f8945c8a4cbace459b
lizhizhou <lzz@meteroi.com>
2015-07-26 03:21:14 -0700

    bug version

--- wechat/wechat.js
+++ wechat/wechat.js
@@ -18,7 +18,51 @@
 var http = require("http"),
 	url  = require("url"),
 	path = require("path"),
-	fs   = require("fs");
-	querystring = require("querystring");  
+	fs   = require("fs"),
+	querystring = require("querystring"),
+        connect = require('connect');
+        wechat = require('wechat');
 
+var config = {
+  token: 'weline',
+  appid: 'wxc61069fb23ba1314',
+  encodingAESKey: 'Fi7m6Qwua3VsBL8K4rc8tZxU5SnZVU3brK2ADLCmSdQ'
+};
 
+var app = connect();
+app.use(connect.query());
+app.use('/wechat', wechat(config, function (req, res, next) {
+  // message is located in req.weixin
+  var message = req.weixin;
+  if (message.FromUserName === 'diaosi') {
+    // reply with text
+    res.reply('hehe');
+  } else if (message.FromUserName === 'text') {
+    // another way to reply with text
+    res.reply({
+      content: 'text object',
+      type: 'text'
+    });
+  } else if (message.FromUserName === 'hehe') {
+    // reply with music
+    res.reply({
+      type: "music",
+      content: {
+        title: "Just some music",
+        description: "I have nothing to lose",
+        musicUrl: "http://mp3.com/xx.mp3",
+        hqMusicUrl: "http://mp3.com/xx.mp3"
+      }
+    });
+  } else {
+    // reply with thumbnails posts
+    res.reply([
+      {
+        title: 'Come to fetch me',
+        description: 'or you want to play in another way ?',
+        picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
+        url: 'http://nodeapi.cloudfoundry.com/'
+      }
+    ]);
+  }
+}));
